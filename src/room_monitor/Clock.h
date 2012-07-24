4535972e90280e4b28b974daf9f63f19ca85a5de
Piotr Gabryanczyk <piotrga@gmail.com>
2012-07-24 21:40:59 +0100

    linking instead of duplicationg

--- src/room_monitor/Clock.h
+++ src/room_monitor/Clock.h
@@ -1,16 +1,1 @@
-#ifndef Clock_h
-#define Clock_h
-
-#include "WProgram.h"
-
-class Clock{
-public:
-  Clock(long startTime);
-  long time();
-  void updateTime();
-private:
-  long _time;  
-  unsigned lastMillis;
-};
-
-#endif
+../arduino-library/clock/Clock.h
